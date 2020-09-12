bf2240a3f3a8aa22de9c6f430dd49fd36388b58a
Arpit Gupta <imarg_3@zoho.com>
2020-09-13 00:50:57 +0530

    User signin screen

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,69 @@
+import React, { useState, useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
+import NavLink from "../components/NavLink";
+
+const SigninScreen = ({ navigation }) => {
+  const { state, signin, clearErrorMessage } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  React.useEffect(() => {
+    const listener = navigation.addListener("blur", () => {
+      clearErrorMessage();
+    });
+
+    return listener;
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign In for Tracker</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Input
+        secureTextEntry
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      {state.errorMessage ? (
+        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
+      ) : null}
+      <Spacer>
+        <Button title="Sign In" onPress={() => signin({ email, password })} />
+      </Spacer>
+      <NavLink
+        routeName="Signup"
+        text="Don't have an account? Sign up instead"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+  errorMessage: {
+    fontSize: 16,
+    color: "red",
+    marginLeft: 15,
+    marginTop: 15,
+  },
+});
+
+export default SigninScreen;
