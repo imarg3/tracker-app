a59a28f43463fb8f1403da59cd4079c427ab7de1
Arpit Gupta <imarg_3@zoho.com>
2020-09-21 20:46:31 +0530

    Use Location hook

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,46 +1,21 @@
-import React, { useEffect, useState, useContext } from "react";
+import "../_mockLocation";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
-import {
-  requestPermissionsAsync,
-  watchPositionAsync,
-  Accuracy,
-} from "expo-location";
+import { useIsFocused } from "@react-navigation/native";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
-import "../_mockLocation";
+import useLocation from "../hooks/useLocation";
 
 const TrackCreateScreen = () => {
   const { addLocation } = useContext(LocationContext);
-  const [err, setErr] = useState(null);
+  // This hook returns `true` if the screen is focused, `false` otherwise
+  const isFocused = useIsFocused();
 
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-
-      await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000, // in seconds
-          distanceInterval: 10, // in meters
-        },
-        (location) => {
-          addLocation(location);
-        }
-      );
-    } catch (e) {
-      setErr(e);
-    }
-  };
-
-  useEffect(() => {
-    startWatching();
-  }, []);
+  // const [err] = useLocation((location) => {addLocation(location)});
+  // Above line equivalent
+  const [err] = useLocation(isFocused, addLocation);
 
   return (
     <SafeAreaView>
