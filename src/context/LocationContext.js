bbcfb66a3442f9894f3ee23762d7d38e6e3d0848
Arpit Gupta <imarg_3@zoho.com>
2020-09-21 19:06:20 +0530

    Project dependencies

--- src/context/LocationContext.js
+++ src/context/LocationContext.js
@@ -14,6 +14,7 @@ const startRecording = (dispatch) => () => {};
 const stopRecording = (dispatch) => () => {};
 
 const addLocation = (dispatch) => (location) => {
+  console.log("Hi there!");
   dispatch({ type: "add_current_location", payload: location });
 };
 
