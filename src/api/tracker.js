7d25802cce8cc4391c4c3c579d6d1a124d723adf
Arpit Gupta <imarg_3@zoho.com>
2020-09-27 00:06:55 +0530

    Add request interceptor for passing user token in http header

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,22 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-community/async-storage";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: "http://192.168.43.232:8080",
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default instance;
