4b10a343ba5c21f6209113b7d8bb258471aee99f
Arpit Gupta <imarg_3@zoho.com>
2020-09-27 00:09:47 +0530

    Add useIsFocused hook to check whether screen is focused or not

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,5 +1,5 @@
 import "../_mockLocation";
-import React, { useContext } from "react";
+import React, { useContext, useCallback } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -7,21 +7,29 @@ import { useIsFocused } from "@react-navigation/native";
 import Map from "../components/Map";
 import { Context as LocationContext } from "../context/LocationContext";
 import useLocation from "../hooks/useLocation";
+import TrackForm from "../components/TrackForm";
 
 const TrackCreateScreen = () => {
-  const { addLocation } = useContext(LocationContext);
+  const { state, addLocation } = useContext(LocationContext);
+  const callback = useCallback(
+    (location) => {
+      addLocation(location, state.recording);
+    },
+    [state.recording]
+  );
   // This hook returns `true` if the screen is focused, `false` otherwise
   const isFocused = useIsFocused();
 
   // const [err] = useLocation((location) => {addLocation(location)});
   // Above line equivalent
-  const [err] = useLocation(isFocused, addLocation);
+  const [err] = useLocation(isFocused || state.recording, callback);
 
   return (
     <SafeAreaView>
       <Text h2>Create a Track</Text>
       <Map />
       {err ? <Text>Please enable location services</Text> : null}
+      <TrackForm />
     </SafeAreaView>
   );
 };
