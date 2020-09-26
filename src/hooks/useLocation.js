eacf5a90b245bf7f82c7abb42b5098ad36b653e1
Arpit Gupta <imarg_3@zoho.com>
2020-09-27 00:04:34 +0530

    Hook to start recording location

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -7,39 +7,46 @@ import {
 
 export default (shouldTrack, callback) => {
   const [err, setErr] = useState(null);
-  const [subscriber, setSubscriber] = useState(null);
-
-  const startWatching = async () => {
-    try {
-      const { granted } = await requestPermissionsAsync();
-
-      if (!granted) {
-        throw new Error("Location permission not granted");
-      }
-
-      const sub = await watchPositionAsync(
-        {
-          accuracy: Accuracy.BestForNavigation,
-          timeInterval: 1000, // in seconds
-          distanceInterval: 10, // in meters
-        },
-        callback
-      );
-      setSubscriber(sub);
-    } catch (e) {
-      setErr(e);
-    }
-  };
 
   useEffect(() => {
+    let subscriber;
+    const startWatching = async () => {
+      try {
+        const { granted } = await requestPermissionsAsync();
+
+        if (!granted) {
+          throw new Error("Location permission not granted");
+        }
+
+        subscriber = await watchPositionAsync(
+          {
+            accuracy: Accuracy.BestForNavigation,
+            timeInterval: 1000, // in seconds
+            distanceInterval: 10, // in meters
+          },
+          callback
+        );
+      } catch (e) {
+        setErr(e);
+      }
+    };
+
     if (shouldTrack) {
       startWatching();
     } else {
-      // stop watching
-      subscriber.remove();
-      setSubscriber(null);
+      if (subscriber) {
+        // stop watching
+        subscriber.remove();
+      }
+      subscriber = null;
     }
-  }, [shouldTrack]);
+
+    return () => {
+      if (subscriber) {
+        subscriber.remove();
+      }
+    };
+  }, [shouldTrack, callback]);
 
   return [err];
 };
