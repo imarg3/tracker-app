1566cc3fadb7cdbc6c8f88d2539dad0aaad6498d
Arpit Gupta <imarg_3@zoho.com>
2020-10-05 23:46:44 +0530

    Add Screen title and icons

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import { Provider as AuthProvider } from "./src/context/AuthContext";
 import ResolveAuthScreen from "./src/screens/ResolveAuthScreen";
 import { Provider as LocationProvider } from "./src/context/LocationContext";
 import { Provider as TrackProvider } from "./src/context/TrackContext";
+import { FontAwesome } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -20,7 +21,11 @@ const Tab = createBottomTabNavigator();
 function TabStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="TrackList" component={TrackListScreen} />
+      <Stack.Screen
+        name="TrackList"
+        component={TrackListScreen}
+        options={{ title: "Tracks" }}
+      />
       <Stack.Screen name="TrackDetail" component={TrackDetailScreen} />
     </Stack.Navigator>
   );
@@ -29,9 +34,36 @@ function TabStack() {
 function BottomTab() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="TabStack" component={TabStack} />
-      <Tab.Screen name="TrackCreate" component={TrackCreateScreen} />
-      <Tab.Screen name="Account" component={AccountScreen} />
+      <Tab.Screen
+        name="TabStack"
+        component={TabStack}
+        options={{
+          title: "Tracks",
+          tabBarIcon: () => {
+            return <FontAwesome name="th-list" size={20} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="TrackCreate"
+        component={TrackCreateScreen}
+        options={{
+          title: "Add Track",
+          tabBarIcon: () => {
+            return <FontAwesome name="plus" size={20} />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountScreen}
+        options={{
+          title: "Account",
+          tabBarIcon: () => {
+            return <FontAwesome name="gear" size={20} />;
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 }
