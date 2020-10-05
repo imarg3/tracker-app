c96f907d7265303573eb7b3b8f95c608cf47abb8
Arpit Gupta <imarg_3@zoho.com>
2020-10-05 23:50:09 +0530

    Add FlatList component to display Track List

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { StyleSheet, Text, FlatList, TouchableOpacity } from "react-native";
+import { StyleSheet, FlatList, TouchableOpacity } from "react-native";
 import { ListItem } from "react-native-elements";
 import { Context as TrackContext } from "../context/TrackContext";
 
