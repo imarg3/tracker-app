68835cef81fc1cb0e7bcca85914145525feef300
Arpit Gupta <imarg_3@zoho.com>
2020-10-05 23:48:23 +0530

    Add FlatList component to display Track List

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,14 +1,41 @@
-import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text, FlatList, TouchableOpacity } from "react-native";
+import { ListItem } from "react-native-elements";
+import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
+  const { state, fetchTracks } = useContext(TrackContext);
+
+  React.useEffect(() => {
+    const listener = navigation.addListener("focus", () => {
+      fetchTracks();
+    });
+
+    return listener;
+  }, [navigation]);
+
   return (
     <>
-      <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-      <Button
-        title="Go to Track Detail"
-        onPress={() => {
-          navigation.navigate("TrackDetail");
+      <FlatList
+        data={state}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("TrackDetail", {
+                  _id: item._id,
+                })
+              }
+            >
+              <ListItem>
+                <ListItem.Title style={{ fontWeight: "bold" }}>
+                  {item.name}
+                </ListItem.Title>
+                <ListItem.Chevron />
+              </ListItem>
+            </TouchableOpacity>
+          );
         }}
       />
     </>
